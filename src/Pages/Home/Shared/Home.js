12abdee8dbaa1f4e3cd2480a22cca7f662e58d0c
unknown <islam15-3879@diu.edu.bd>
2022-05-10 13:44:24 +0600

    card-added

--- src/Pages/Home/Shared/Home.js
+++ src/Pages/Home/Shared/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Bnner from '../Bnner';
-
+import Info from '../Info';
 const Home = () => {
     return (
         <div>
             <Bnner></Bnner>
+            <Info></Info>
         </div>
     );
 };
