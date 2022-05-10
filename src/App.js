00d6df8b623316da91de5f8dbcbbbe8dc363271a
unknown <islam15-3879@diu.edu.bd>
2022-05-10 13:02:04 +0600

    navbar-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Shared/Home';
 import Navbar from './Pages/Home/Shared/Navbar';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>thi is login page</h2>
+        </div>
+    );
+};
+
+export default Login;
