6edc67cbd23c32d276d6d7ea63c174cf24dea708
unknown <islam15-3879@diu.edu.bd>
2022-05-12 16:25:53 +0600

    mongodb-and-localhost-connected

--- src/Pages/Appoinment/AvailableAppoinment.js
+++ src/Pages/Appoinment/AvailableAppoinment.js
@@ -11,7 +11,7 @@ const AvailableAppoinment = ({date,setDate}) => {
 
 
     useEffect(()=>{
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
         .then(res=>res.json())
         .then(data=>setServices(data))
     },[])
