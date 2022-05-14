6e63708e595df78f31aa0116198cc3e82e4d808a
unknown <islam15-3879@diu.edu.bd>
2022-05-14 07:33:39 +0600

    login-page-done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -29,30 +29,57 @@ const Login = () => {
                placeholder="Your Email"
                 class="input input-bordered w-full max-w-xs"
                 {...register("email",  {
+                    required:{
+                      value:true,
+                      message : 'Email is Required'
+                    },
                     pattern: {
-                      value: /[A-Za-z]{3}/,
-                      message: 'error message' // JS only: <p>error message</p> TS only support string
+                      value: /[a-z0-9]+@[a-z]+\.edu\.[a-z]{2,3}/,
+                      message: 'Provide a valid valid'
                     }
                   })}
                 />
              <label class="label">
-             <span class="label-text-alt">Alt label</span>
-    
+             {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+            {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
                </label>
             </div>
-           <input  />
-            {errors.firstName?.type === 'required' && "First name is required"}
-      
-            <input {...register("lastName", { required: true })} />
-           {errors.lastName && "Last name is required"}
-      
-           <input type="submit" />
+           <div class="form-control w-full max-w-xs">
+                 <label class="label">
+                  <span class="label-text">Password</span>
+    
+             </label>
+             <input
+              type="password"
+               placeholder="Your Password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password",  {
+                    required:{
+                      value:true,
+                      message : 'password is Required'
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'Must be 6 character or longer'
+                    }
+                  })}
+                />
+             <label class="label">
+             {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+            {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+               </label>
+            </div>
+
+
+
+           
+           <input className='btn w-full max-w-xs' type="submit"  value='Login'/>
             </form>
 
            <div className="divider">OR</div>
            <button
             onClick={()=>signInWithGoogle()} 
-           className="btn btn-outline">Continue With Google</button>
+           className="btn  btn-outline">Continue With Google</button>
     </div>
   </div>
 </div>
