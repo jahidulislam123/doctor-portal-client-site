b59dfe8811ec61f192e7d473fa2420cf8760deaf
unknown <islam15-3879@diu.edu.bd>
2022-05-17 23:37:07 +0600

    jwt-verified

--- src/Pages/Dashboard/MyAppoinment.js
+++ src/Pages/Dashboard/MyAppoinment.js
@@ -1,17 +1,33 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 
 const MyAppoinment = () => {
-
+    const navigate =useNavigate();
     const [user]=useAuthState(auth);
     const [appoinments,setAppoinment]=useState([]);
     useEffect(()=>{
        if(user){
-        fetch(`http://localhost:5000/booking?patient=${user.email}`)
-        .then(res=>res.json())
-        .then(data=>setAppoinment(data))
+        fetch(`http://localhost:5000/booking?patient=${user.email}`,{
+            method:'GET',
+            headers:{
+                'authorization':`Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res=>{
+            console.log('res',res)
+            if(res.status===401 || res.status===403){
+              signOut(auth);
+              localStorage.removeItem('accessToken')
+              navigate('/');
+            }
+           return res.json()})
+        .then(data=>{
+
+            setAppoinment(data)})
        }
     },[user])
     return (
